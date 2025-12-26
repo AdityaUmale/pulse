11d027537774c037b5ab5c3ea1619d42fcd5301d
Aditya Umale <adityaumale@Adityas-MacBook-Pro.local>
2025-12-26 17:09:35 +0530

    auth

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,21 @@
- import { PrismaClient} from "@/generated/prisma";
+import { PrismaClient } from "@/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
- const globalForPrisma = global as unknown as {
+const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
- }
+};
 
- const prisma = globalForPrisma.prisma || new PrismaClient();
+function createPrismaClient() {
+    const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+    });
+    const adapter = new PrismaPg(pool);
+    return new PrismaClient({ adapter });
+}
 
- if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;    
+const prisma = globalForPrisma.prisma || createPrismaClient();
 
- export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
